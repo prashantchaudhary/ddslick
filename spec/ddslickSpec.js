df56100984b72c4c20e47a4f0d37addee9d09e3d
Tim Wingfield <tim@timwingfield.com>
2013-10-03 17:19:01 -0400

    fixed typo

--- spec/ddslickSpec.js
+++ spec/ddslickSpec.js
@@ -1,9 +1,27 @@
 describe('ddSlick', function(){
   context('using <select> to render', function(){
-    it('should be true', function(){
-      var t = true;
-      expect(t).toEqual(true);
+    beforeEach(function(){
+      buildDropdown();
+    });
+
+    afterEach(function(){
+      $("#the-dropdown").remove();
+    });
+    
+    
+    it('does some stuff', function(){
+      var subject = $('#the-dropdown');
+      subject.ddslick({
+        imagePosition: "right"
+      });
+      console.log(subject);
     });
   });
 });
 
+var buildDropdown = function(){
+  $("<select id='the-dropdown'></select>").appendTo('body');
+  $("<option data-imagesrc='spongebob.png' value='spongebob'>Spongebob</option>").appendTo('#the-dropdown');
+  $("<option data-imagesrc='patrick.png' value='patrick'>Patrick</option>").appendTo('#the-dropdown');
+  $("<option data-imagesrc='squidard.png' value='squidard'>Squidard</option>").appendTo('#the-dropdown');
+};
